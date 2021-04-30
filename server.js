dfd383a59ea3ab7611926d2ec589e5d78c5604f6
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-30 15:39:46 +0000

    (feat): cmp app

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { v4: uuidV4 } = require("uuid");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", (_, res) => {
+app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
 
@@ -19,6 +19,10 @@ io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
   });
 });
 
